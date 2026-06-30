0b25e037985732996fcf845c3035e8683bf037ce
hyunjun <chlguswns96@gmail.com>
2026-06-30 16:17:49 +0900

    first commit getsum 기능개발

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pytest
 
-
 class Calc:
     # 이 곳에 코드를 작성
+    def getSum(self, a, b):
+        return a + b
     pass
 
 
 # 테스트 케이스 작성
 def test_sample():
-    assert 1 == 1
-    pytest.fail()
+    c = Calc()
+
+    assert c.getSum(4, 8) == 1
