57a210489af24a2a2e90449e0225de393c56e9e2
seungju.cho <sgmath12@gmail.com>
2026-06-30 16:09:36 +0900

    Add getDivide and getSumSum methods with tests

--- main.py
+++ main.py
@@ -3,10 +3,30 @@ import pytest
 
 class Calc:
     # 이 곳에 코드를 작성
-    pass
+
+    def getDivide(self, a, b):
+        return a / b
+
+    def getSumSum(self, a, b, c):
+        return a + b + c
 
 
 # 테스트 케이스 작성
-def test_sample():
-    assert 1 == 1
-    pytest.fail()
+def test_getDivide():
+    calc = Calc()
+    assert calc.getDivide(10, 2) == 5
+    assert calc.getDivide(7, 2) == 3.5
+    assert calc.getDivide(-6, 3) == -2
+
+
+def test_getDivide_by_zero():
+    calc = Calc()
+    with pytest.raises(ZeroDivisionError):
+        calc.getDivide(5, 0)
+
+
+def test_getSumSum():
+    calc = Calc()
+    assert calc.getSumSum(1, 2, 3) == 6
+    assert calc.getSumSum(0, 0, 0) == 0
+    assert calc.getSumSum(-1, 2, -3) == -2
